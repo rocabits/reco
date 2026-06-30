22f751f0b3455cc95f8c47270cc4bd514d4b73ad
rocabits <roca.jlr@gmail.com>
2026-06-30 12:29:00 +0200

    cambio desde opencode

--- generate-icons.ps1
+++ generate-icons.ps1
@@ -6,39 +6,35 @@ function New-Icon($size, $outputPath) {
   $g.SmoothingMode = 'HighQuality'
   $g.InterpolationMode = 'HighQualityBicubic'
 
-  $green = [System.Drawing.Color]::FromArgb(46, 204, 113)
   $white = [System.Drawing.Color]::FromArgb(255, 255, 255)
+  $green = [System.Drawing.Color]::FromArgb(46, 204, 113)
 
-  $bgBrush = New-Object System.Drawing.SolidBrush($green)
+  # White background
+  $bgBrush = New-Object System.Drawing.SolidBrush($white)
   $g.FillRectangle($bgBrush, 0, 0, $size, $size)
 
-  $darkGreen = [System.Drawing.Color]::FromArgb(39, 174, 96)
-  $circleBrush = New-Object System.Drawing.SolidBrush($darkGreen)
-  $m = [int]($size * 0.04)
-  $g.FillEllipse($circleBrush, $m, $m, $size - 2 * $m, $size - 2 * $m)
-
-  $innerBrush = New-Object System.Drawing.SolidBrush($green)
-  $im = [int]($size * 0.08)
-  $g.FillEllipse($innerBrush, $im, $im, $size - 2 * $im, $size - 2 * $im)
-
   $s = [double]($size) / 100.0
-  $wb = New-Object System.Drawing.SolidBrush($white)
+  $gb = New-Object System.Drawing.SolidBrush($green)
 
+  # Shopping cart body (basket)
   $pts = New-Object System.Drawing.PointF[] 4
   $pts[0] = New-Object System.Drawing.PointF ([float](27*$s)), ([float](38*$s))
   $pts[1] = New-Object System.Drawing.PointF ([float](73*$s)), ([float](38*$s))
   $pts[2] = New-Object System.Drawing.PointF ([float](78*$s)), ([float](66*$s))
   $pts[3] = New-Object System.Drawing.PointF ([float](22*$s)), ([float](66*$s))
-  $g.FillPolygon($wb, $pts)
+  $g.FillPolygon($gb, $pts)
 
-  $g.FillRectangle($wb, [float](27*$s), [float](24*$s), [float](5*$s), [float](14*$s))
-  $g.FillRectangle($wb, [float](27*$s), [float](24*$s), [float](17*$s), [float](5*$s))
-  $g.FillEllipse($wb, [float]((30-6)*$s), [float]((70-6)*$s), [float](12*$s), [float](12*$s))
-  $g.FillEllipse($wb, [float]((70-6)*$s), [float]((70-6)*$s), [float](12*$s), [float](12*$s))
+  # Handle
+  $g.FillRectangle($gb, [float](27*$s), [float](24*$s), [float](5*$s), [float](14*$s))
+  $g.FillRectangle($gb, [float](27*$s), [float](24*$s), [float](17*$s), [float](5*$s))
+
+  # Wheels
+  $g.FillEllipse($gb, [float]((30-6)*$s), [float]((70-6)*$s), [float](12*$s), [float](12*$s))
+  $g.FillEllipse($gb, [float]((70-6)*$s), [float]((70-6)*$s), [float](12*$s), [float](12*$s))
 
   $bmp.Save($outputPath, [System.Drawing.Imaging.ImageFormat]::Png)
 
-  $wb.Dispose(); $innerBrush.Dispose(); $circleBrush.Dispose(); $bgBrush.Dispose()
+  $gb.Dispose(); $bgBrush.Dispose()
   $g.Dispose(); $bmp.Dispose()
 }
 
